68a7ea97e52d7914fcf22ec6e250d06093e1519f
PolinaShneider <beatrix.linkoln@gmail.com>
2019-06-19 10:06:27 +0300

    Update LanguageService usage

--- monaco/chartsWorker.ts
+++ monaco/chartsWorker.ts
@@ -9,12 +9,12 @@ export class ChartsWorker {
 
   private _ctx: IWorkerContext;
   private _languageId: string;
-  private languageService: LanguageService<ResourcesProvider>;
+  private languageService: LanguageService;
 
   constructor(ctx: IWorkerContext, createData: ICreateData) {
     this._ctx = ctx;
     this._languageId = createData.languageId;
-    this.languageService = new LanguageService<ResourcesProvider>(new ResourcesProvider());
+    this.languageService = new LanguageService(new ResourcesProvider());
   }
 
   public doComplete(
