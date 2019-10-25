3fff621157158a34c793611911b43861d1010580
PolinaShneider <beatrixlinkoln@gmail.com>
2019-10-25 11:07:54 +0300

    Some refactoring

--- src/editorActions.ts
+++ src/editorActions.ts
@@ -1,4 +1,4 @@
-import {editor} from "monaco-editor-core";
+import { editor } from "monaco-editor-core";
 
 export interface EditorOptions {
     editorId?: string;
@@ -8,18 +8,33 @@ export interface EditorOptions {
     fontFamily?: string;
     fontSize?: number;
     tabSize?: number;
-    onChange?: () => {},
-    onSave?: () => {}
+    onChange?: () => {};
+    onSave?: () => {};
+    gridEnabled?: boolean;
 }
 
-const FN_NOOP = () => {
-};
-const SETTINGS_STORAGE_KEY = 'ChartsEditorSettings';
+interface Subscriber {
+    Subject: HTMLElement;
+    Action: () => void;
+}
+
+// tslint:disable-next-line: no-empty
+const FN_NOOP = () => { };
+const SETTINGS_STORAGE_KEY = "ChartsEditorSettings";
 const DEFAULT_TAB_SIZE: number = 2;
 
 export class EditorActions {
+    /**
+     * Some actions not connected with editor instance which should be performed on save
+     * @param onSave
+     */
+    // tslint:disable-next-line: ban-types
+    public static saveEditorContents(onSave: Function = FN_NOOP) {
+        onSave();
+    }
     public chartsEditor: editor.ICodeEditor;
     public gridEnabled: boolean = false;
+    private subscribers: Subscriber[] = [];
 
     public initEditor(options: EditorOptions): void {
         /** Editor is already initialized */
@@ -27,19 +42,22 @@ export class EditorActions {
             return;
         }
 
-        const editorId = options.editorId || 'editorHolder';
-        const language = options.language || 'axibaseCharts';
-        const value = options.value || '';
-        const theme = options.theme || 'chartsTheme';
-        const fontFamily = options.fontFamily || 'Source Code Pro';
+        const editorId = options.editorId || "editorHolder";
+        const language = options.language || "axibaseCharts";
+        const value = options.value || "";
+        const theme = options.theme || "chartsTheme";
+        const fontFamily = options.fontFamily || "Source Code Pro";
         const fontSize = options.fontSize || 12;
         const tabSize = options.tabSize || DEFAULT_TAB_SIZE;
+        this.gridEnabled = options.gridEnabled || false;
+
         /**
          * Editor onchange (type, paste text) callback
          */
         const onChange = (
             options.onChange instanceof Function
         ) ? options.onChange : FN_NOOP;
+
         /**
          * Editor save callback
          */
@@ -51,51 +69,33 @@ export class EditorActions {
          * Write editor's settings to localStorage
          */
         localSettings.init({
-            fontSize: fontSize
+            fontSize,
+            gridEnabled: /grid-display\s*=\strue/gm.test(value)
         });
 
         const editorHolder = document.getElementById(editorId);
 
         if (!editorHolder) {
-            console.log('Editor holder with id «' + editorId + '» not found');
+            console.log("Editor holder with id «" + editorId + "» not found");
             return;
         }
 
         this.chartsEditor = monaco.editor.create(
             editorHolder,
             {
-                theme,
-                value,
-                language,
-                fontSize: localSettings.get('fontSize') || fontSize,
                 fontFamily,
+                fontSize: localSettings.get("fontSize") || fontSize,
+                language,
+                lineDecorationsWidth: 0.5,
+                lineNumbersMinChars: 4,
                 minimap: {
                     enabled: false
                 },
-                lineNumbersMinChars: 4,
-                lineDecorationsWidth: 0.5,
-                scrollBeyondLastLine: false
+                scrollBeyondLastLine: false,
+                theme,
+                value,
             }
         );
-
-        document.getElementById("portal").addEventListener("load", () => {
-            let gridEnabled = false;
-            const editorValue = this.getEditorValue();
-            console.log("load event triggered")
-            const gridButton = document.getElementById("gridBtn");
-
-            if (!gridButton.classList.contains("loaded")) {
-                gridEnabled = /grid-display\s*=\strue/gm.test(editorValue);
-                gridButton.classList.add("loaded");
-            } else {
-                if (!gridButton.classList.contains("manual-enable")) {
-                    gridEnabled = !/grid-display\s*=\sfalse/gm.test(editorValue) && editorValue !== options.value;
-                } else {
-                    gridEnabled = gridButton.classList.contains("active");
-                }
-            }
-            connect(gridEnabled).then(() => console.log("loaded successfully from constructor"));
-        });
 
         /**
          * Create controls to increase/decrease editor's font-size
@@ -105,18 +105,18 @@ export class EditorActions {
 
         editorHolder.appendChild(fontControls);
 
-        editorHolder.addEventListener('mouseover', function() {
-            fontControls.style.opacity = '1';
+        editorHolder.addEventListener("mouseover", () => {
+            fontControls.style.opacity = "1";
         });
 
-        editorHolder.addEventListener('mouseout', function() {
-            fontControls.style.opacity = '0';
+        editorHolder.addEventListener("mouseout", () => {
+            fontControls.style.opacity = "0";
         });
 
         /**
          * Set editor tabSize
          */
-        this.chartsEditor.getModel().updateOptions({tabSize});
+        this.chartsEditor.getModel().updateOptions({ tabSize });
 
         /**
          * Activate callback for editor content changes
@@ -126,22 +126,10 @@ export class EditorActions {
         EditorActions.saveEditorContents.bind(null, onSave);
     }
 
-    /**
-     * Some actions not connected with editor instance which should be performed on save
-     * @param onSave
-     */
-    public static saveEditorContents(onSave: Function = FN_NOOP) {
-        onSave();
-    }
-
     public toggleGrid(): void {
         this.gridEnabled = !this.gridEnabled;
-        connect(this.gridEnabled).then(() => console.log("grid was toggled"));
-    }
-
-    public setGridValue(value: boolean): void {
-        console.log("setting grid value")
-        connect(value).then(() => console.log("grid enabled"));
+        sendMessage(this.gridEnabled).then(() => this.notifyControl());
+        localSettings.update("gridEnabled", this.gridEnabled);
     }
 
     /**
@@ -164,19 +152,18 @@ export class EditorActions {
         text = (endLine > 1) ? "\n\n" + text : text;
 
         const edits = {
-            identifier: {major: 1, minor: 1},
-            range: new monaco.Range(endLine + 1, endCharacter, endLine + 1, endCharacter),
-            text: text,
             /**
              * If start & end of the range aren't distant enough to contain config's text
              * (they obviously aren't), automatically expand range
              */
-            forceMoveMarkers: true
+            forceMoveMarkers: true,
+            identifier: { major: 1, minor: 1 },
+            range: new monaco.Range(endLine + 1, endCharacter, endLine + 1, endCharacter),
+            text
         };
 
-        this.chartsEditor.executeEdits('onPaste', [edits]);
-    };
-
+        this.chartsEditor.executeEdits("onPaste", [edits]);
+    }
     /**
      * Set model tab size
      * @param tabSize
@@ -184,7 +171,7 @@ export class EditorActions {
     public setTabSize(tabSize: number): void {
         tabSize = tabSize || DEFAULT_TAB_SIZE;
         if (this.chartsEditor) {
-            this.chartsEditor.getModel().updateOptions({tabSize});
+            this.chartsEditor.getModel().updateOptions({ tabSize });
         }
     }
 
@@ -195,22 +182,20 @@ export class EditorActions {
     public getEditorValue(): string {
         /** Check if editor has been initialized */
         if (!this.chartsEditor) {
-            return '';
+            return "";
         }
 
         return this.chartsEditor.getValue();
-    };
-
+    }
     /**
      * Format editor's config
      */
     public formatEditorContents() {
         /** Check if editor has been initialized */
         if (this.chartsEditor) {
-            this.chartsEditor.trigger(this.getEditorValue(), 'editor.action.formatDocument', {});
+            this.chartsEditor.trigger(this.getEditorValue(), "editor.action.formatDocument", {});
         }
-    };
-
+    }
     /**
      * Trigger editor redraw
      */
@@ -218,8 +203,7 @@ export class EditorActions {
         if (this.chartsEditor) {
             this.chartsEditor.layout();
         }
-    };
-
+    }
     /**
      * Brings focus to textarea
      */
@@ -227,50 +211,68 @@ export class EditorActions {
         if (this.chartsEditor) {
             this.chartsEditor.focus();
         }
-    };
+    }
+
+    public subscribeControl(button: HTMLElement, action: () => void): void {
+        console.log("subscribed a button");
+        console.log(this, "this from subscribe")
+        this.subscribers.push({
+            Action: action,
+            Subject: button
+        });
+    }
+
+    private notifyControl(): void {
+        console.log("subscribers are notified");
+        console.log(this)
+        for (let { Subject, Action } of this.subscribers) {
+            const func = Action.bind(Subject);
+            func();
+        }
+    }
 
     /**
      * Create controls buttons to increase/decrease editor's font-size
      * @return {HTMLElement}
      */
     private createFontControls(): HTMLElement {
-        const container = document.createElement('div');
-        container.classList.add('font-controls');
+        const container = document.createElement("div");
+        container.classList.add("font-controls");
 
-        const decreaseFontBtn = document.createElement('div');
-        decreaseFontBtn.className += ' btn-minus control-button';
+        const decreaseFontBtn = document.createElement("div");
+        decreaseFontBtn.className += " btn-minus control-button";
 
-        decreaseFontBtn.addEventListener('click', () => {
-            this.chartsEditor.getAction('editor.action.fontZoomOut').run().then(
+        decreaseFontBtn.addEventListener("click", () => {
+            this.chartsEditor.getAction("editor.action.fontZoomOut").run().then(
                 () => {
-                    return localSettings.update('fontSize', this.chartsEditor.getConfiguration().fontInfo.fontSize);
+                    return localSettings.update("fontSize", this.chartsEditor.getConfiguration().fontInfo.fontSize);
                 }
             );
         });
 
-        const increaseFontBtn = document.createElement('div');
-        increaseFontBtn.className += ' btn-plus control-button';
+        const increaseFontBtn = document.createElement("div");
+        increaseFontBtn.className += " btn-plus control-button";
 
-        increaseFontBtn.addEventListener('click', () => {
-            this.chartsEditor.getAction('editor.action.fontZoomIn').run().then(
+        increaseFontBtn.addEventListener("click", () => {
+            this.chartsEditor.getAction("editor.action.fontZoomIn").run().then(
                 () => {
-                    return localSettings.update('fontSize', this.chartsEditor.getConfiguration().fontInfo.fontSize);
+                    return localSettings.update("fontSize", this.chartsEditor.getConfiguration().fontInfo.fontSize);
                 }
             );
         });
 
-        const separator = document.createElement('div');
-        separator.className += ' btn-label control-button';
+        const separator = document.createElement("div");
+        separator.className += " btn-label control-button";
 
         container.appendChild(decreaseFontBtn);
         container.appendChild(separator);
         container.appendChild(increaseFontBtn);
 
         return container;
-    };
+    }
 }
 
-function connect(value: boolean = false) {
+function sendMessage(value: boolean = false) {
     const gridButton = document.getElementById("gridBtn");
     return new Promise((resolve, reject) => {
         let stopped = false;
@@ -282,7 +284,7 @@ function connect(value: boolean = false) {
 
         const connected = function ok(event: MessageEvent) {
             stopped = true;
-            if (event.data.type === "axiToggleGridResponse" && event.data.value === value) {
+            if (event.data.type === "axiToggleGridResponse") {
                 gridButton.classList.toggle("active", event.data.value);
             }
             window.removeEventListener("message", ok);
@@ -309,7 +311,7 @@ function connect(value: boolean = false) {
  * Helper functions to keep editor settings in localStorage
  */
 const localSettings = {
-    init: function(settings: EditorOptions) {
+    init(settings: EditorOptions) {
         try {
             if (localStorage.getItem(SETTINGS_STORAGE_KEY)) {
                 // Settings were already saved
@@ -318,26 +320,26 @@ const localSettings = {
 
             localStorage.setItem(SETTINGS_STORAGE_KEY, JSON.stringify(settings));
         } catch (e) {
-            console.warn('Couldn\'t set initial editor\'s settings:', e.message);
+            console.warn("Couldn't set initial editor's settings:", e.message);
         }
     },
 
-    update: function(name: string, setting: any) {
+    update(name: string, setting: any) {
         try {
             const settings = JSON.parse(localStorage.getItem(SETTINGS_STORAGE_KEY));
 
             settings[name] = setting;
             localStorage.setItem(SETTINGS_STORAGE_KEY, JSON.stringify(settings));
         } catch (e) {
-            console.warn('Couldn\'t update editor\'s settings:', e.message);
+            console.warn("Couldn't update editor's settings:", e.message);
         }
     },
 
-    get: function(name: string) {
+    get(name: string) {
         try {
             return JSON.parse(localStorage.getItem(SETTINGS_STORAGE_KEY))[name];
         } catch (e) {
-            console.warn('Couldn\'t read editor\'s setting,', name, ':', e.message);
+            console.warn("Couldn't read editor's setting,", name, ":", e.message);
         }
 
         return null;
@@ -347,7 +349,7 @@ const localSettings = {
 /**
  * Run save on Ctrl+S or Cmd+S
  */
-window.addEventListener('keydown', (event) => {
+window.addEventListener("keydown", (event) => {
     if (event.keyCode === 83 && (event.ctrlKey || event.metaKey)) {
 
         EditorActions.saveEditorContents();
@@ -355,4 +357,9 @@ window.addEventListener('keydown', (event) => {
         event.preventDefault();
         event.stopPropagation();
     }
+});
+
+document.getElementById("portal").addEventListener("load", () => {
+    const gridEnabled = localSettings.get("gridEnabled") || false;
+    sendMessage(gridEnabled).then(() => console.log("portal connected after reload"));
 });
